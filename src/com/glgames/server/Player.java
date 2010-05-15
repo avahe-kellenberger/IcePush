1b0bbc35086dbb58e4baa586e703870f24a5779e
anonymous <anonymous@anonymous>
2010-05-15 22:51:44 +0000

    FIXED SERVER FAILURE

--- src/com/glgames/server/Player.java
+++ src/com/glgames/server/Player.java
@@ -1,24 +1,16 @@
 package com.glgames.server;
 
-import static com.glgames.shared.Opcodes.BAD_VERSION;
 import static com.glgames.shared.Opcodes.END_MOVE;
+import static com.glgames.shared.Opcodes.KEEP_ALIVE;
 import static com.glgames.shared.Opcodes.LOGOUT;
 import static com.glgames.shared.Opcodes.MOVE_REQUEST;
 import static com.glgames.shared.Opcodes.NEW_PLAYER;
 import static com.glgames.shared.Opcodes.PLAYER_DIED;
 import static com.glgames.shared.Opcodes.PLAYER_LOGGED_OUT;
 import static com.glgames.shared.Opcodes.PLAYER_MOVED;
-import static com.glgames.shared.Opcodes.SUCCESS_LOG;
-import static com.glgames.shared.Opcodes.TOO_MANY_PL;
-import static com.glgames.shared.Opcodes.USER_IN_USE;
-import static com.glgames.shared.Opcodes.KEEP_ALIVE;
 
 import java.awt.Rectangle;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
 
-import com.glgames.shared.Opcodes;
 import com.glgames.shared.PacketBuffer;
 
 public class Player {
@@ -35,60 +27,12 @@ public class Player {
 	public Rectangle area;
 	public PacketBuffer pbuf;
 
-	private String username;
+	public String username;
 	public boolean connected;
 	private int moveDir = -1;
 
-	public Player(Socket s) {
-		try {
-			InputStream in = s.getInputStream();
-			OutputStream out = s.getOutputStream();
-			int version = in.read();
-			if (version != Opcodes.VERSION) {
-				out.write(BAD_VERSION); // bad version
-				out.flush();
-				return;
-			}
-			int len = in.read();
-			byte[] strb = new byte[len];
-			in.read(strb);
-
-			username = new String(strb);
-
-			for (int k = 0; k < Server.players.length; k++)
-				if (Server.players[k] != null) {
-					String user = Server.players[k].username;
-					if (user != null && user.equals(username)) {
-						out.write(USER_IN_USE); // name in use
-						out.flush();
-						return;
-					}
-				}
-			int index = -1;
-			for (int k = 0; k < Server.players.length; k++)
-				if (Server.players[k] == null) {
-					index = k;
-					break;
-				}
-			if (index == -1) {
-				out.write(TOO_MANY_PL);
-				out.flush();
-				return;
-			}
-
-			id = index;
-			type = index % 2;
-
-			initPosition();
-			out.write(SUCCESS_LOG); // success
-			out.write(id);
-			out.flush();
-
-			pbuf = new PacketBuffer(s);
-			Server.players[id] = this;
-			connected = true;
-			System.out.println("Player logged in: " + username + ", id: " + id);
-
+	public Player() {
+		try{
 			for (Player plr : Server.players) {
 				try {
 					if (plr == null)
@@ -125,7 +69,7 @@ public class Player {
 		pbuf.endPacket();
 	}
 
-	private void initPosition() {
+	void initPosition() {
 		area = new Rectangle(0, 0, 48, 48);
 		area.x = (int) (Math.random() * (400 - 48));
 		area.y = (int) (Math.random() * (400 - 48));
--- src/com/glgames/server/Server.java
+++ src/com/glgames/server/Server.java
@@ -1,7 +1,17 @@
 package com.glgames.server;
 
+import static com.glgames.shared.Opcodes.BAD_VERSION;
+import static com.glgames.shared.Opcodes.SUCCESS_LOG;
+import static com.glgames.shared.Opcodes.TOO_MANY_PL;
+import static com.glgames.shared.Opcodes.USER_IN_USE;
+
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+
+import com.glgames.shared.Opcodes;
+import com.glgames.shared.PacketBuffer;
 
 public class Server implements Runnable {
 	public static boolean DEBUG = false;
@@ -22,7 +32,7 @@ public class Server implements Runnable {
 					System.out.println("Client accepted, socket: "
 							+ s.toString());
 					s.setTcpNoDelay(true);
-					new Player(s); // ETC ETC
+					loginPlayer(s);
 				}
 				updatePlayers();
 
@@ -33,6 +43,61 @@ public class Server implements Runnable {
 				}
 			}
 		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void loginPlayer(Socket s) {
+		Player p = new Player();
+		try {
+			InputStream in = s.getInputStream();
+			OutputStream out = s.getOutputStream();
+			int version = in.read();
+			if (version != Opcodes.VERSION) {
+				out.write(BAD_VERSION); // bad version
+				out.flush();
+				return;
+			}
+			int len = in.read();
+			byte[] strb = new byte[len];
+			in.read(strb);
+
+			p.username = new String(strb);
+
+			for (int k = 0; k < players.length; k++)
+				if (players[k] != null) {
+					String user = players[k].username;
+					if (user != null && user.equals(p.username)) {
+						out.write(USER_IN_USE); // name in use
+						out.flush();
+						return;
+					}
+				}
+			int index = -1;
+			for (int k = 0; k < players.length; k++)
+				if (players[k] == null) {
+					index = k;
+					break;
+				}
+			if (index == -1) {
+				out.write(TOO_MANY_PL);
+				out.flush();
+				return;
+			}
+
+			p.id = index;
+			p.type = index % 2;
+
+			p.initPosition();
+			out.write(SUCCESS_LOG); // success
+			out.write(p.id);
+			out.flush();
+
+			p.pbuf = new PacketBuffer(s);
+			p.connected = true;
+			players[p.id] = p;
+			System.out.println("Player logged in: " + p.username + ", id: " + p.id);
+		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
