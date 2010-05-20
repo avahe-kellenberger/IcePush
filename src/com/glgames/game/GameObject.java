c287902b754888cac1fc4cca1de8854545d489ea
tekk <tekk@strictfp.com>
2010-05-20 00:11:57 +0000

    ?????

---
+++ src/com/glgames/game/GameObject.java
@@ -0,0 +1,5 @@
+package com.glgames.game;
+
+public class GameObject {
+
+}
---
+++ src/com/glgames/game/Player3D.java
@@ -0,0 +1,10 @@
+package com.glgames.game;
+
+public class Player3D extends Object3D {
+	public String username;
+	public int deaths;
+	
+	public Player3D(int type) {
+		super(type);
+	}
+}
