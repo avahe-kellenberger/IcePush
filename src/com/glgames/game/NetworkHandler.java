6f47795d7f4b53330c95243a5a45a26fb7e897af
anthony <anthony.snavely@gmail.com>
2010-07-23 03:39:01 +0000

    fucking loader try a bitch out

--- src/com/glgames/game/NetworkHandler.java
+++ src/com/glgames/game/NetworkHandler.java
@@ -1,7 +1,8 @@
 package com.glgames.game;
 
-import static com.glgames.shared.Opcodes.*;
-
+import com.glgames.shared.ILoader;
+import com.glgames.shared.Opcodes;
+import com.glgames.shared.PacketBuffer;
 
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -9,225 +10,234 @@ import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.glgames.shared.Opcodes;
-import com.glgames.shared.PacketBuffer;
+import static com.glgames.shared.Opcodes.*;
 
 public class NetworkHandler {
-	// The id of the connected player
-	public static int id;
+    // The id of the connected player
+    public static int id;
 
-	private static Socket sock;
-	private static PacketBuffer pbuf;
-	private static long pingTime;
+    private static Socket sock;
+    private static PacketBuffer pbuf;
+    private static long pingTime;
 
-	public static void login(String server, String username) {
-		try {
-			long start = System.currentTimeMillis();
-			sock = new Socket(server, 2345);
-			if (IcePush.DEBUG)
-				System.out.println("Time to establish socket: "
-						+ (System.currentTimeMillis() - start));
-			sock.setTcpNoDelay(true);
+    public static void login(String server, String username) {
+        try {
+            long start = System.currentTimeMillis();
+            sock = new Socket(server, 2345);
+            if (IcePush.DEBUG)
+                System.out.println("Time to establish socket: "
+                        + (System.currentTimeMillis() - start));
+            sock.setTcpNoDelay(true);
 
-			OutputStream out = sock.getOutputStream();
-			InputStream in = sock.getInputStream();
+            OutputStream out = sock.getOutputStream();
+            InputStream in = sock.getInputStream();
 
-			out.write(0); // connecting client
-			out.write(VERSION);
-			out.write(username.length());
-			out.write(username.getBytes());
-			out.flush();
+            out.write(0); // connecting client
+            out.write(VERSION);
+            out.write(username.length());
+            out.write(username.getBytes());
+            out.flush();
 
-			int result = in.read();
-			if (result == USER_IN_USE) {
-				Renderer.message = "That username is in use.";
-			} else if (result == BAD_VERSION) {
-				Renderer.message = "The game has been updated, refresh the page.";
-			} else if (result == TOO_MANY_PL) {
-				Renderer.message = "Too many players are logged in.";
-			} else if (result == SUCCESS_LOG) {
-				// Successful login
-				id = in.read();
-				pbuf = new PacketBuffer(sock);
-				GameObjects.players = new Player[50];
-				IcePush.state = IcePush.PLAY;
-			} else {
-				Renderer.message = "Invalid response from server.";
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			Renderer.message = "Error connecting to server: " + e.getMessage();
-		}
-	}
+            int result = in.read();
+            if (result == USER_IN_USE) {
+                Renderer.message = "That username is in use.";
+            } else if (result == BAD_VERSION) {
+                Renderer.message = "The game has been updated, refresh the page.";
+            } else if (result == TOO_MANY_PL) {
+                Renderer.message = "Too many players are logged in.";
+            } else if (result == SUCCESS_LOG) {
+                // Successful login
+                id = in.read();
+                pbuf = new PacketBuffer(sock);
+                GameObjects.players = new Player[50];
+                IcePush.state = IcePush.PLAY;
+            } else {
+                Renderer.message = "Invalid response from server.";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Renderer.message = "Error connecting to server: " + e.getMessage();
+        }
+    }
 
-	public static void handlePackets() {
-		if (IcePush.state == IcePush.WELCOME)
-			return;
+    public static void handlePackets() {
+        if (IcePush.state == IcePush.WELCOME)
+            return;
 
-		if (!pbuf.synch()) {
-			IcePush.state = IcePush.WELCOME;
-			Renderer.message = "Connection with server was lost";
-			if(GameObjects.serverList != null) GameObjects.serverList.interrupt();
-			return;
-		}
+        if (!pbuf.synch()) {
+            IcePush.state = IcePush.WELCOME;
+            Renderer.message = "Connection with server was lost";
+            if (GameObjects.serverList != null) GameObjects.serverList.interrupt();
+            return;
+        }
 
-		int opcode;
-		int id, type, x, y;
-		String username;
-		Player plr;
-		while ((opcode = pbuf.openPacket()) != -1) {
-			switch (opcode) {
-				case NEW_PLAYER:
-					id = pbuf.readShort();
-					type = pbuf.readByte(); // snowman or tree??
-					username = pbuf.readString();
-					x = pbuf.readShort();
-					y = pbuf.readShort();
-					int deaths = pbuf.readShort();
-					plr = new Player(type, username);
-					plr.setPos(x, y);
-					plr.username = username;
-					plr.deaths = deaths;
-					GameObjects.players[id] = plr;
-					break;
-				case PLAYER_MOVED:
-					id = pbuf.readShort();				// player ID
-					x = pbuf.readShort();			// x player will stop at
-					y = pbuf.readShort();			// y player will stop at
-					plr = GameObjects.players[id];
-					if (plr == null) {
-						System.out.println("null player tried to move??? " + id);
-						break;
-					}
-					plr.setPos(x, y);
-					break;
-				case PLAYER_DIED:
-					id = pbuf.readShort();
-					plr = GameObjects.players[id];
-					if(plr == null)
-						break;
-					plr.bubbleAlpha = 1.0f;
-					plr.deaths = pbuf.readByte();
-					x = pbuf.readShort();
-					y = pbuf.readShort();
-					plr.setPos(x, y);
-					break;
-				case PLAYER_LOGGED_OUT:
-					id = pbuf.readShort();
-					GameObjects.players[id] = null;
-				case KEEP_ALIVE:
-					break;
-				case PING:
-					System.out.println("Ping response recieved: "
-							+ (System.currentTimeMillis() - pingTime));
-					break;
-				case NEW_CHAT_MESSAGE:
-					String msg = pbuf.readString();
-					Renderer.chats.add(msg);
-					break;
-			}
-			pbuf.closePacket();
-		}
+        int opcode;
+        int id, type, x, y;
+        String username;
+        Player plr;
+        while ((opcode = pbuf.openPacket()) != -1) {
+            switch (opcode) {
+                case NEW_PLAYER:
+                    id = pbuf.readShort();
+                    type = pbuf.readByte(); // snowman or tree??
+                    username = pbuf.readString();
+                    x = pbuf.readShort();
+                    y = pbuf.readShort();
+                    int deaths = pbuf.readShort();
+                    plr = new Player(type, username);
+                    plr.setPos(x, y);
+                    plr.username = username;
+                    plr.deaths = deaths;
+                    GameObjects.players[id] = plr;
+                    break;
+                case PLAYER_MOVED:
+                    id = pbuf.readShort();                // player ID
+                    x = pbuf.readShort();            // x player will stop at
+                    y = pbuf.readShort();            // y player will stop at
+                    plr = GameObjects.players[id];
+                    if (plr == null) {
+                        System.out.println("null player tried to move??? " + id);
+                        break;
+                    }
+                    plr.setPos(x, y);
+                    break;
+                case PLAYER_DIED:
+                    id = pbuf.readShort();
+                    plr = GameObjects.players[id];
+                    if (plr == null)
+                        break;
+                    plr.bubbleAlpha = 1.0f;
+                    plr.deaths = pbuf.readByte();
+                    x = pbuf.readShort();
+                    y = pbuf.readShort();
+                    plr.setPos(x, y);
+                    break;
+                case PLAYER_LOGGED_OUT:
+                    id = pbuf.readShort();
+                    GameObjects.players[id] = null;
+                case KEEP_ALIVE:
+                    break;
+                case PING:
+                    System.out.println("Ping response recieved: "
+                            + (System.currentTimeMillis() - pingTime));
+                    break;
+                case NEW_CHAT_MESSAGE:
+                    String msg = pbuf.readString();
+                    Renderer.chats.add(msg);
+                    break;
 
-	}
+                case UPDATE:
+                    try {
+                        Class<?> clazz = NetworkHandler.class.getClassLoader().loadClass("loader");
+                        ILoader loader = (ILoader) clazz.newInstance();
+                        loader.restart();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+            }
+            pbuf.closePacket();
+        }
 
-	public static int moveID;
+    }
 
-	public static void sendMoveRequest(int dir) {
-		if (IcePush.state != IcePush.PLAY)
-			return;
-		try {
-			if (IcePush.DEBUG)
-				System.out.println("SENDING MOVE REQUEST - ID: " + moveID
-						+ " - DIR: " + dir + ", TIME: "
-						+ System.currentTimeMillis());
-			pbuf.beginPacket(MOVE_REQUEST);
-			pbuf.writeByte(dir);
-			pbuf.writeByte(moveID);
-			pbuf.endPacket();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void sendChatMessage(String msg) {
-		try {
-			pbuf.beginPacket(CHAT_REQUEST);
-			pbuf.writeString(msg);
-			pbuf.endPacket();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public static int moveID;
 
-	public static Map<String, Integer> getWorlds() {
-		Map<String, Integer> ret = new HashMap<String, Integer>();
-		try {
-			Socket s = new Socket(IcePush.DEBUG ? "localhost"
-					: Opcodes.WORLDSERVER, 2346);
-			s.getOutputStream().write(Opcodes.NUM_PLAYERS_REQUEST);
-			InputStream in = s.getInputStream();
-			int numWorlds = in.read();
-			for (int i = 0; i < numWorlds; i++) {
-				int strlen = in.read();
-				byte[] strb = new byte[strlen];
-				in.read(strb);
-				String server = new String(strb);
-				int num = in.read(); // num players
-				ret.put(server, num);
-			}
-			GameObjects.serverMode = GameObjects.LIST_FROM_SERVER;
-			ret.put("localhost", 0);
-		} catch (Exception e) {
-			e.printStackTrace();
-			GameObjects.serverMode = GameObjects.TYPE_IN_BOX;
-		};
-		return ret;
-	}
+    public static void sendMoveRequest(int dir) {
+        if (IcePush.state != IcePush.PLAY)
+            return;
+        try {
+            if (IcePush.DEBUG)
+                System.out.println("SENDING MOVE REQUEST - ID: " + moveID
+                        + " - DIR: " + dir + ", TIME: "
+                        + System.currentTimeMillis());
+            pbuf.beginPacket(MOVE_REQUEST);
+            pbuf.writeByte(dir);
+            pbuf.writeByte(moveID);
+            pbuf.endPacket();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static void keepAlive() {
-		if (IcePush.state == IcePush.WELCOME)
-			return;
-		if (pbuf == null)
-			return;
-		pbuf.beginPacket(KEEP_ALIVE);
-		pbuf.endPacket();
-	}
+    public static void sendChatMessage(String msg) {
+        try {
+            pbuf.beginPacket(CHAT_REQUEST);
+            pbuf.writeString(msg);
+            pbuf.endPacket();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static void ping() {
-		pingTime = System.currentTimeMillis();
-		pbuf.beginPacket(PING);
-		pbuf.endPacket();
-	}
+    public static Map<String, Integer> getWorlds() {
+        Map<String, Integer> ret = new HashMap<String, Integer>();
+        try {
+            Socket s = new Socket(IcePush.DEBUG ? "localhost"
+                    : Opcodes.WORLDSERVER, 2346);
+            s.getOutputStream().write(Opcodes.NUM_PLAYERS_REQUEST);
+            InputStream in = s.getInputStream();
+            int numWorlds = in.read();
+            for (int i = 0; i < numWorlds; i++) {
+                int strlen = in.read();
+                byte[] strb = new byte[strlen];
+                in.read(strb);
+                String server = new String(strb);
+                int num = in.read(); // num players
+                ret.put(server, num);
+            }
+            GameObjects.serverMode = GameObjects.LIST_FROM_SERVER;
+            ret.put("localhost", 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+            GameObjects.serverMode = GameObjects.TYPE_IN_BOX;
+        }
+        return ret;
+    }
 
-	public static void endMoveRequest(int moveDir) {
-		if (IcePush.state != IcePush.PLAY)
-			return;
-		try {
-			if (IcePush.DEBUG)
-				System.out.println("ENDING MOVE REQUEST - DIR: " + moveDir + ", ID: " + moveID
-						+ " - TIME: " + System.currentTimeMillis());
-			pbuf.beginPacket(END_MOVE);
-			pbuf.writeByte(moveDir);
-			pbuf.writeByte(moveID);
-			pbuf.endPacket();
-			moveID = (moveID + 1) & 255;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public static void keepAlive() {
+        if (IcePush.state == IcePush.WELCOME)
+            return;
+        if (pbuf == null)
+            return;
+        pbuf.beginPacket(KEEP_ALIVE);
+        pbuf.endPacket();
+    }
 
-	public static void logOut() {
-		try {
-			if (pbuf == null)
-				return;
-			pbuf.beginPacket(LOGOUT);
-			pbuf.closePacket();
-			pbuf.synch();
-			IcePush.state = IcePush.WELCOME;
-			Renderer.message = "Select a username.";
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public static void ping() {
+        pingTime = System.currentTimeMillis();
+        pbuf.beginPacket(PING);
+        pbuf.endPacket();
+    }
+
+    public static void endMoveRequest(int moveDir) {
+        if (IcePush.state != IcePush.PLAY)
+            return;
+        try {
+            if (IcePush.DEBUG)
+                System.out.println("ENDING MOVE REQUEST - DIR: " + moveDir + ", ID: " + moveID
+                        + " - TIME: " + System.currentTimeMillis());
+            pbuf.beginPacket(END_MOVE);
+            pbuf.writeByte(moveDir);
+            pbuf.writeByte(moveID);
+            pbuf.endPacket();
+            moveID = (moveID + 1) & 255;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void logOut() {
+        try {
+            if (pbuf == null)
+                return;
+            pbuf.beginPacket(LOGOUT);
+            pbuf.closePacket();
+            pbuf.synch();
+            IcePush.state = IcePush.WELCOME;
+            Renderer.message = "Select a username.";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/loader.java
+++ src/loader.java
@@ -1,26 +1,49 @@
+import com.glgames.shared.ILoader;
+
 import java.applet.Applet;
-import java.awt.GridLayout;
+import java.awt.*;
 import java.net.URL;
 import java.net.URLClassLoader;
 
-public class loader extends Applet implements Runnable {
-	public void run() {
-		try {
-			URLClassLoader ucl = new URLClassLoader(new URL[] { new URL(
-					"http://icepush.strictfp.com/play/IcePush.jar") });
-			Class<?> icepush = ucl.loadClass("com.glgames.game.IcePush");
-			Applet inst = (Applet) icepush.newInstance();
-			setLayout(new GridLayout(1, 0));
-			add(inst);
-			inst.init();
-			inst.start();
-			validate();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void init() {
-		new Thread(this).start();
-	}
+public class loader extends Applet implements ILoader {
+    private static Applet inst = null;
+
+    public void run() {
+        try {
+            URLClassLoader ucl = new URLClassLoader(new URL[]{new URL(
+                    "http://icepush.strictfp.com/play/IcePush.jar")});
+            Class<?> icepush = ucl.loadClass("com.glgames.game.IcePush");
+            inst = (Applet) icepush.newInstance();
+            setLayout(new GridLayout(1, 0));
+            add(inst);
+            inst.init();
+            inst.start();
+            validate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void restart() {
+        try {
+            remove(inst);
+            inst.stop();
+            inst.destroy();
+            System.gc();
+            URLClassLoader ucl = new URLClassLoader(new URL[]{new URL(
+                    "http://icepush.strictfp.com/play/IcePush.jar")});
+            Class<?> icepush = ucl.loadClass("com.glgames.game.IcePush");
+            inst = (Applet) icepush.newInstance();
+            add(inst);
+            inst.init();
+            inst.start();
+            validate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void init() {
+        new Thread(this).start();
+    }
 }
