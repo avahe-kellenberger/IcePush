5091c202344cf34cf7fee671c7a6462d256394de
tekk <tekk@strictfp.com>
2010-07-23 03:32:20 +0000

    updated build scripts to include data/

--- mkjar.bat
+++ mkjar.bat
@@ -1,10 +1,4 @@
-
-
 cd bin
-
-jar cvfm ../IcePush.jar ../manifest.txt com/glgames/game/* com/glgames/shared/* images/*
-
+jar cvfm ../IcePush.jar ../manifest.txt com/glgames/game/* com/glgames/shared/* images/* data/*
 cd ../
-
 pscp IcePush.jar cobol@strictfp.com:/var/www/docs/icepush.strictfp.com/public/play/IcePush.jar
-
--- mkjar.sh
+++ mkjar.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd bin
-jar cvfm ../IcePush.jar ../manifest.txt com/glgames/game/* com/glgames/shared/* images/*
+jar cvfm ../IcePush.jar ../manifest.txt com/glgames/game/* com/glgames/shared/* images/* data/*
 cd ../
 #jarsigner IcePush.jar "mykey"
 scp IcePush.jar cobol@strictfp.com:/var/www/docs/icepush.strictfp.com/public/play/IcePush.jar
-
