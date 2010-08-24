8e3b43db75d6268986c15a21536acab8db3066cc
linkmaster03 <linkmaster03@gmx.com>
2010-08-24 16:12:08 +0000

    Unversion the JAR build script because it is developer independent. This delete preserves the file locally.

--- mkjar.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd bin
-jar cvfm ../IcePush.jar ../manifest.txt com/glgames/game/* com/glgames/shared/* images/* data/*
-cd ../
-#jarsigner IcePush.jar "mykey"
-scp IcePush.jar cobol@strictfp.com:/var/www/docs/icepush.strictfp.com/public/play/IcePush.jar
