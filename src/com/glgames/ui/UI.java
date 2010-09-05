19c4f670c720b586684d2733687ee5432218e235
linkmaster03 <linkmaster03@gmx.com>
2010-09-04 22:35:38 -0400

    Add new UI components for map editor screen

--- src/com/glgames/ui/UI.java
+++ src/com/glgames/ui/UI.java
@@ -3,17 +3,27 @@ package com.glgames.ui;
 public class UI extends Container {
 	public Container welcomeScreenContainer;
 	public Container helpScreenContainer;
+	public Container mapEditorScreenContainer;
+
 	public Container instructionsContainer;
 	public Container dataEntryContainer;
 	public Container buttonContainer;
 	public Container helpTextContainer;
+	public Container toolContainer;
+
+	public Label networkStatus;
 	public TextBox serverTextBox;
 	public TextBox usernameTextBox;
 	public Button loginButton;
 	public Button helpButton;
+	public Button mapEditorButton;
 	public Button backButton;
 	public Button logoutButton;
-	public Label networkStatus;
+	public MapCanvas mapCanvas;
+	public Button selectButton;
+	public Button lineButton;
+	public Button quadButton;
+	public Button cubicButton;
 
 	public UI(int width, int height) {
 		super(0, 0, width, height);
@@ -21,35 +31,50 @@ public class UI extends Container {
 		String[] instructionsText = new String[] {"Push the other players off the ice!", "Try not to fall off!"};
 		String[] helpText = new String[] {"Arrow keys - move", "Q - logout", "2 - 2D view", "3 - 3D view", "C - chat"};
 
+		// Create screen containers
 		welcomeScreenContainer = new Container(0, 0, width, height);
 		helpScreenContainer = new Container(0, 0, width, height);
+		mapEditorScreenContainer = new Container(0, 0, width, height);
 
+		// Create sub-containers
 		instructionsContainer = new Container(0, 140, width, 70);
 		dataEntryContainer = new Container(315, 250, 170, 65);
-		buttonContainer = new Container(295, 345, 210, 25);
+		buttonContainer = new Container(295, 330, 210, 85);
 		helpTextContainer = new Container(0, 150, width, 200);
+		toolContainer = new Container(100, 450, 600, 25);
 
+		// Create welcome screen components
 		networkStatus = new Label("Select a username.");
 		serverTextBox = new TextBox(170, 20);
 		serverTextBox.setCaption("Server: ");
 		usernameTextBox = new TextBox(170, 20);
 		usernameTextBox.focus();
 		usernameTextBox.setCaption("Username: ");
-
 		loginButton = new Button(100, 25);
 		loginButton.setCaption("Login");
 		helpButton = new Button(100, 25);
 		helpButton.setCaption("Help");
+		mapEditorButton = new Button(150, 25);
+		mapEditorButton.setCaption("Map Editor");
+
+		// Create help screen components
 		backButton = new Button(350, 345, 100, 25);
 		backButton.setCaption("Back");
 		logoutButton = new Button(690, 2, 100, 27);
 		logoutButton.setCaption("Logout");
 
-		// Pack the components into the containers
-		welcomeScreenContainer.setLayout(Container.Layout.FIXED);
-		welcomeScreenContainer.addChild(instructionsContainer);
-		welcomeScreenContainer.addChild(dataEntryContainer);
-		welcomeScreenContainer.addChild(buttonContainer);
+		// Create map screen components
+		mapCanvas = new MapCanvas(0, 0, width, height - 40);
+		selectButton = new Button(100, 25);
+		selectButton.setCaption("Select");
+		lineButton = new Button(100, 25);
+		lineButton.setCaption("Line");
+		quadButton = new Button(100, 25);
+		quadButton.setCaption("Quad");
+		cubicButton = new Button(100, 25);
+		cubicButton.setCaption("Cubic");
+
+		// Pack the welcome screen
 		instructionsContainer.setLayout(Container.Layout.VERTICAL);
 		for (String line : instructionsText)
 			instructionsContainer.addChild(new Label(line));
@@ -57,20 +82,40 @@ public class UI extends Container {
 		dataEntryContainer.setLayout(Container.Layout.VERTICAL);
 		dataEntryContainer.addChild(serverTextBox);
 		dataEntryContainer.addChild(usernameTextBox);
+		buttonContainer.setLayout(Container.Layout.VERTICAL);
 		buttonContainer.addChild(loginButton);
 		buttonContainer.addChild(helpButton);
-		helpScreenContainer.setLayout(Container.Layout.FIXED);
-		helpScreenContainer.addChild(backButton);
-		helpScreenContainer.addChild(helpTextContainer);
+		buttonContainer.addChild(mapEditorButton);
+		welcomeScreenContainer.setLayout(Container.Layout.FIXED);
+		welcomeScreenContainer.addChild(instructionsContainer);
+		welcomeScreenContainer.addChild(dataEntryContainer);
+		welcomeScreenContainer.addChild(buttonContainer);
+
+		// Pack the help screen
 		helpTextContainer.setLayout(Container.Layout.VERTICAL);
 		for (String line : helpText)
 			helpTextContainer.addChild(new Label(line));
+		helpScreenContainer.setLayout(Container.Layout.FIXED);
+		helpScreenContainer.addChild(backButton);
+		helpScreenContainer.addChild(helpTextContainer);
+
+		// Pack the map screen
+		toolContainer.addChild(selectButton);
+		toolContainer.addChild(lineButton);
+		toolContainer.addChild(quadButton);
+		toolContainer.addChild(cubicButton);
+		mapEditorScreenContainer.setLayout(Container.Layout.FIXED);
+		mapEditorScreenContainer.addChild(mapCanvas);
+		mapEditorScreenContainer.addChild(toolContainer);
+
+		// Pack the root UI
 		this.setLayout(Container.Layout.FIXED);
 		this.addChild(welcomeScreenContainer);
 		this.addChild(helpScreenContainer);
+		this.addChild(mapEditorScreenContainer);
 		this.addChild(logoutButton);
 
-		// Recursively make every component visible
+		// Show nothing except the welcome screen
 		this.setVisible(true);
 		welcomeScreenContainer.setVisibleRecursive(true);
 	}
