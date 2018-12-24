9e6d670bb6162514bcb1d7d04b57081d74ff9255
Avahe Kellenberger <avahe@protonmail.ch>
2018-12-23 23:52:54 -0500

    Slightly restructured index.css and added a 'Help' button to `HomeScreen`.
    The css class should allow for easy placement of DOM elements atop the HTMLCanvasElement `canvas`.

--- client/src/ts/icepush/scene/HomeScreen.ts
+++ client/src/ts/icepush/scene/HomeScreen.ts
@@ -4,14 +4,43 @@ import {ClientAssets} from "../asset/ClientAssets";
 export class HomeScreen extends Scene {
 
     private readonly btnLogin: HTMLButtonElement;
+    private readonly btnHelp: HTMLButtonElement;
 
     constructor() {
         super();
         this.btnLogin = document.createElement('button');
         this.btnLogin.innerHTML = 'Login';
-        this.btnLogin.className = 'btn-login';
-        this.btnLogin.addEventListener('click', () => console.log(`Clicked \'${this.btnLogin.innerText}\'`));
+        this.btnLogin.className = 'on-canvas';
+        this.btnLogin.style.top = '73%';
+        this.btnLogin.style.left = '50%';
+        this.btnLogin.style.transform = 'translate(-112%, -50%)';
+        this.btnLogin.addEventListener('click', this.login.bind(this));
+
+        this.btnHelp = document.createElement('button');
+        this.btnHelp.innerHTML = 'Help';
+        this.btnHelp.className = 'on-canvas';
+        this.btnHelp.style.top = '73%';
+        this.btnHelp.style.left = '50%';
+        this.btnHelp.style.transform = 'translate(12%, -50%)';
+        this.btnHelp.addEventListener('click', this.showHelp.bind(this));
     }
+
+    /**
+     * Attempt to log in.
+     */
+    private login(): boolean {
+        console.log(`Clicked \'${this.btnLogin.innerText}\'`);
+        return false;
+    }
+
+    /**
+     * Show the help screen.
+     */
+    private showHelp(): void {
+        console.log(`Clicked \'${this.btnHelp.innerText}\'`);
+    }
+
+    // region Overridden functions
 
     /**
      * Retrieves the container element on the DOM.
@@ -29,7 +58,9 @@ export class HomeScreen extends Scene {
      */
     public onSwitchedToCurrent(): void {
         // Call getter every time, in case the DOM is modified.
-        this.getDOMContainer().appendChild(this.btnLogin);
+        const container: HTMLElement = this.getDOMContainer();
+        container.appendChild(this.btnLogin);
+        container.appendChild(this.btnHelp);
     }
 
     /**
@@ -37,7 +68,9 @@ export class HomeScreen extends Scene {
      */
     public onSwitchedFromCurrent(): void {
         // Call getter every time, in case the DOM is modified.
-        this.getDOMContainer().removeChild(this.btnLogin);
+        const container: HTMLElement = this.getDOMContainer();
+        container.removeChild(this.btnLogin);
+        container.removeChild(this.btnHelp);
     }
 
     /**
@@ -48,5 +81,7 @@ export class HomeScreen extends Scene {
         ctx.drawImage(ClientAssets.IMAGE_BACKGROUND, 0, 0);
         super.render(ctx);
     }
+
+    // endregion
 
 }
