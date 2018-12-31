443be1a5c3eeb37b29d33330e38c2c383b019720
Avahe Kellenberger <avahe@protonmail.ch>
2018-12-31 03:46:58 -0500

    Added more NetworkEvents.

---
+++ client/src/ts/icepush/net/events/PlayerLoggedOutEvent.ts
@@ -0,0 +1,41 @@
+import {NetworkEvent} from "../NetworkEvent";
+import {OPCode} from "../OPCode";
+import {PositionedBuffer} from "../PositionedBuffer";
+
+export class PlayerLoggedOutEvent extends NetworkEvent {
+
+    public readonly playerID: number;
+    private readonly BINARY_SIZE: number = 2;
+
+    /**
+     * @param playerID The ID of the player which logged out.
+     */
+    constructor(playerID: number);
+    constructor(buffer: PositionedBuffer);
+    constructor(bufferOrID: PositionedBuffer|number) {
+        super();
+        this.playerID = typeof bufferOrID === 'number' ? bufferOrID : bufferOrID.readInt16BE();
+    }
+
+    /**
+     * @override
+     */
+    public getEventSize(): number {
+        return this.BINARY_SIZE;
+    }
+
+    /**
+     * @override
+     */
+    public getOPCode(): OPCode {
+        return OPCode.PLAYER_LOGGED_OUT;
+    }
+
+    /**
+     * @override
+     */
+    public write(buffer: PositionedBuffer): void {
+        buffer.writeInt16BE(this.playerID);
+    }
+
+}
