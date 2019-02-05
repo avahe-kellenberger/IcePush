260ed6ea77153970650c17b0948522fbec952078
Avahe Kellenberger <avahe@protonmail.ch>
2019-02-04 20:47:05 -0500

    Updated ThreadedTask to use a more precise and user friendly wait time for task looping.

--- src/net/threesided/util/ThreadedTask.java
+++ src/net/threesided/util/ThreadedTask.java
@@ -10,7 +10,7 @@ public class ThreadedTask {
     private final Supplier<Boolean> condition;
     private final Runnable task;
     private final boolean loop;
-    private final long targetLoopRate;
+    private final long loopRateNanos;
 
     private Thread taskThread;
 
@@ -26,28 +26,37 @@ public class ThreadedTask {
      * Executes the given task on a new thread.
      * @param task The task to execute.
      * @param loop If the task should loop.
-     * @param loopRateMillis Target frequency in milliseconds to execute the given task, if looped.
+     * @param loopRate Target frequency in seconds to execute the given task, if looped.
      */
-    public ThreadedTask(final Runnable task, final Supplier<Boolean> condition, final boolean loop, final long loopRateMillis) {
+    public ThreadedTask(final Runnable task, final Supplier<Boolean> condition,
+                        final boolean loop, final double loopRate) {
         this.task = task;
         this.condition = condition;
         this.loop = loop;
-        this.targetLoopRate = loopRateMillis;
+        this.loopRateNanos = (long) loopRate * 1000000000;
     }
 
     /**
      * Loops the given task so long as the `condition` is met and the thread isn't interrupted.
      */
     private void loopTask() {
-        long lastExecutionTimeMillis = System.currentTimeMillis();
+        long lastExecutionTime = System.nanoTime();
         while (!this.taskThread.isInterrupted() && this.condition.get()) {
             this.task.run();
-            final long currentTimeMillis = System.currentTimeMillis();
-            final long elapsedMillis = currentTimeMillis - lastExecutionTimeMillis;
+            final long currentTime = System.nanoTime();
+            final long elapsedTime = currentTime - lastExecutionTime;
 
             try {
-                this.taskThread.wait(Math.max(0, this.targetLoopRate - elapsedMillis));
-                lastExecutionTimeMillis = currentTimeMillis;
+                // Calculate precise wait time between each loop.
+                final long remainingWaitNanos = this.loopRateNanos - elapsedTime;
+                if (remainingWaitNanos > 0) {
+                    final long waitMillis = (long) Math.floor(remainingWaitNanos / 1000000d);
+                    final long waitNanos = remainingWaitNanos - (waitMillis * 1000000);
+                    this.taskThread.wait(waitMillis, (int) waitNanos);
+                }
+
+                // Update last execution time stamp.
+                lastExecutionTime = currentTime;
             } catch (final InterruptedException ex) {
                 ex.printStackTrace();
             }
