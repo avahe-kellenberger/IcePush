6d69bc9dbd9a53435731232f729f2792da17e32b
Avahe Kellenberger <avahe@protonmail.ch>
2019-02-05 13:55:49 -0500

    Refactored Player.

--- src/net/threesided/server/Player.java
+++ src/net/threesided/server/Player.java
@@ -2,161 +2,116 @@ package net.threesided.server;
 
 import static net.threesided.shared.Constants.*;
 
-import java.awt.geom.Path2D;
 import java.util.ArrayList;
 
 import net.threesided.server.net.PacketMapper;
 import net.threesided.server.physics2d.Circle;
+import net.threesided.shared.Constants;
 import net.threesided.shared.PacketBuffer;
+import net.threesided.shared.Vector2D;
 
 public class Player extends Circle {
 
-    public int id;
-    public int lives;
-    public int type;
-    public String username;
-    public boolean connected;
-    public boolean readVer;
-    public boolean readName;
-    public boolean isDead = false;
-    public int timeDead = 0;
+    private int id;
+    private int lives;
+    private int type;
+    private String username;
+    private boolean connected;
+    private boolean readVer;
+    private boolean readName;
+    private boolean isDead = false;
+    private int timeDead = 0;
 
-    PacketBuffer pbuf;
+    private PacketBuffer packetBuffer;
 
-    public boolean logOut;
-    String chatMessage;
+    private boolean logOut;
+    private String chatMessage;
 
     // Length of arrays can be adjusted for more precision
-    public static final float[] sines = new float[256];
-    public static final float[] cosines = new float[256];
+    private static final float[] sines = new float[256];
+    private static final float[] cosines = new float[256];
 
     static {
         double d = (2.0 * Math.PI) / 256;
         for (int k = 0; k < 256; k++) {
-            sines[k] = (float) Math.sin(k * d);
-            cosines[k] = (float) Math.cos(k * d);
+            Player.sines[k] = (float) Math.sin(k * d);
+            Player.cosines[k] = (float) Math.cos(k * d);
         }
     }
 
-    public Player(PacketBuffer pb) {
+    public Player(PacketBuffer packetBuffer) {
         super(20);
-        mass = 5;
-        pbuf = pb;
-        id = -1;
+        this.setMass(5);
+        this.packetBuffer = packetBuffer;
+        this.id = -1;
     }
 
     // Sends this player the login information for newly logged in player p
     public void notifyLogin(Player p) {
-        pbuf.beginPacket(NEW_PLAYER);
-        pbuf.writeShort(p.id);
-        pbuf.writeByte(p.type);
-        pbuf.writeString(p.username);
-        pbuf.writeByte(p.lives);
-        pbuf.endPacket();
+        packetBuffer.beginPacket(NEW_PLAYER);
+        packetBuffer.writeShort(p.id);
+        packetBuffer.writeByte(p.type);
+        packetBuffer.writeString(p.username);
+        packetBuffer.writeByte(p.lives);
+        packetBuffer.endPacket();
     }
 
     // Notifies this player that player p has moved
     public void handleMove(Player p) {
-        pbuf.beginPacket(PLAYER_MOVED);
-        pbuf.writeShort(p.id);
-        pbuf.writeShort((int) p.position.getX());
-        pbuf.writeShort((int) p.position.getY());
-        pbuf.endPacket();
+        packetBuffer.beginPacket(Constants.PLAYER_MOVED);
+        packetBuffer.writeShort(p.id);
+
+        final Vector2D location = p.getLocation();
+        packetBuffer.writeShort((int) location.x);
+        packetBuffer.writeShort((int) location.y);
+        packetBuffer.endPacket();
     }
 
     public void writePendingChats(ArrayList<String> chats) {
         for (String s : chats) {
-            pbuf.beginPacket(NEW_CHAT_MESSAGE);
-            pbuf.writeString(s);
-            pbuf.endPacket();
-        }
-    }
-
-    /**
-     * Resets this players position, making sure it intersects none of the players in the array and
-     * is within path.
-     *
-     * @param players
-     * @param path
-     */
-    public void initPosition(Player[] players, Path2D path) {
-        boolean good;
-        do {
-            good = true;
-
-            do {
-                position.set(Math.random() * 744, Math.random() * 422);
-            } while (!path.contains(position.getX(), position.getY()));
-
-            for (Player p : players) {
-                if (p == null || p == this) {
-                    continue;
-                }
-
-                double dx = position.getX() - p.position.getX();
-                double dy = position.getY() - p.position.getY();
-
-                double sum = this.radius + p.radius;
-
-                double dist = dx * dx + dy * dy;
-
-                if (dist < sum * sum) {
-                    good = false;
-                }
-            }
-
-        } while (!good);
-
-        velocity.set(0, 0);
-        acceleration.set(0, 0);
-
-        for (Player p : players) {
-            if (p == null) {
-                continue;
-            }
-            p.handleMove(this);
+            packetBuffer.beginPacket(Constants.NEW_CHAT_MESSAGE);
+            packetBuffer.writeString(s);
+            packetBuffer.endPacket();
         }
     }
 
     // Notify this player of how much time is remaining in the current round
     public void notifyNewRound(int time) {
-        pbuf.beginPacket(BEGIN_ROUND);
-        pbuf.writeShort(time);
-        pbuf.endPacket();
+        packetBuffer.beginPacket(BEGIN_ROUND);
+        packetBuffer.writeShort(time);
+        packetBuffer.endPacket();
     }
 
     public void notifyVictoryLap(int time) {
-        pbuf.beginPacket(BEGIN_VICTORY_LAP);
-        pbuf.writeShort(time);
-        pbuf.endPacket();
+        packetBuffer.beginPacket(BEGIN_VICTORY_LAP);
+        packetBuffer.writeShort(time);
+        packetBuffer.endPacket();
     }
 
     public void updateWinners(final byte[] winners) {
-        pbuf.beginPacket(UPDATE_WINNER);
-        pbuf.writeByte(winners.length);
+        packetBuffer.beginPacket(UPDATE_WINNER);
+        packetBuffer.writeByte(winners.length);
         for (final byte b : winners) {
-            pbuf.writeByte(b);
+            packetBuffer.writeByte(b);
         }
-        pbuf.endPacket();
+        packetBuffer.endPacket();
     }
 
     public boolean processIncomingPackets() {
-        if (!pbuf.synch()) return false;
-        PacketMapper.handlePackets(
-                pbuf, this); // TODO: Figure out whether this dependency is appropriate or not
+        if (!packetBuffer.sync()) {
+            return false;
+        }
+        // TODO: Figure out whether this dependency is appropriate or not
+        PacketMapper.handlePackets(packetBuffer, this);
         return true;
     }
 
     public void MOVE_REQUEST(int moveDir) {
         setBit(moveDir);
-        // if(Serv er.DEBUG) System.out.println("GOT MOVE REQUEST - DIR: " + moveDir + " - ID = " +
-        // moveId + " , TIME: " + System.currentTimeMillis());
     }
 
     public void END_MOVE() {
         clearBit();
-        // if(Serv er.DEBUG) System.out.println("END MOVE REQUEST - ID = " + moveId + " - TIME = " +
-        // System.currentTimeMillis());
     }
 
     public void LOGOUT() {
@@ -168,38 +123,39 @@ public class Player extends Circle {
     }
 
     public void PING() {
-        pbuf.beginPacket(PING);
-        pbuf.endPacket();
+        packetBuffer.beginPacket(PING);
+        packetBuffer.endPacket();
     }
 
-    public void loggedOut(Player p) { // Tell this player that player p logged out
-        pbuf.beginPacket(PLAYER_LOGGED_OUT);
-        pbuf.writeShort(p.id);
-        pbuf.endPacket();
+    /**
+     * Tell this player that player p logged out
+     * @param p
+     */
+    public void loggedOut(Player p) {
+        packetBuffer.beginPacket(PLAYER_LOGGED_OUT);
+        packetBuffer.writeShort(p.id);
+        packetBuffer.endPacket();
     }
 
-    public void updateLives(Player p) { // Notify this player how many lives p has remaining
-        pbuf.beginPacket(PLAYER_DIED);
-        pbuf.writeShort(p.id);
-        pbuf.writeByte(p.lives);
-        pbuf.endPacket();
+    /**
+     * Notify this player how many lives p has remaining
+     * @param p
+     */
+    public void updateLives(Player p) {
+        packetBuffer.beginPacket(PLAYER_DIED);
+        packetBuffer.writeShort(p.id);
+        packetBuffer.writeByte(p.lives);
+        packetBuffer.endPacket();
     }
-
-    // Tells this player to reset deaths for player p to 0
-    /*public void resetDeaths(Player p) {
-    	pbuf.beginPacket(PLAYER_DIED);
-    	pbuf.writeShort(p.id);
-    	pbuf.writeByte(0);					// Number of times died
-    	pbuf.endPacket();
-    } */
 
     private void setBit(int bit) {
         // scale down by 2 so that the values are between
         // -0.5 and +0.5, like the original version
-        acceleration.set(sines[bit & 0xff] / 2, cosines[bit & 0xff] / 2);
+        this.setAcceleration(new Vector2D(sines[bit & 0xff] / 2, cosines[bit & 0xff] / 2));
     }
 
     private void clearBit() {
-        acceleration.set(0, 0);
+        this.setAcceleration(Vector2D.ZERO);
     }
+
 }
