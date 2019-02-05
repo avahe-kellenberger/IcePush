c281ce06906755e5be9c1420eb5d245fa2018965
Avahe Kellenberger <avahe@protonmail.ch>
2019-02-04 20:47:47 -0500

    Updated formatting and documentation of InterthreadQueue.
    This class will need more work in the future, after we figure out its exact functionality.

--- src/net/threesided/shared/InterthreadQueue.java
+++ src/net/threesided/shared/InterthreadQueue.java
@@ -1,34 +1,50 @@
 package net.threesided.shared;
 
-/* Utility class that is thread safe if no writes are performed concurrently and no reads are performed concurrently.
-A read and a write my safely occur concurrently. The traditional scenario here is to have one thread writing based
-on some outside event, and another thread that consumes the written elements as part of a processing loop. 		*/
-
+/**
+ * Utility class that is thread safe if no writes are performed concurrently and no reads are performed concurrently.
+ * A read and a write may safely occur concurrently. The traditional scenario here is to have one thread writing based
+ * on some outside event, and another thread that consumes the written elements as part of a processing loop.
+ */
 public class InterthreadQueue<T> {
+
     private Node<T> head, tail;
 
+    /**
+     *
+     */
     public InterthreadQueue() {
-        head = tail = new Node<T>();
+        this.head = this.tail = new Node<>();
     }
 
+    /**
+     * Pulls an item off the queue.
+     * @return The item taken from the queue.
+     */
     public T pull() {
-        Node<T> tt =
-                tail.prev; // Local reference created to ensure that the value does not change
-                           // concurrently after being read.
-        if (tt == null) return null; // If this is non-null, we know that tail != head
-        tail = tt;
+        // Local reference created to ensure that the value does not change concurrently after being read.
+        Node<T> tt = tail.prev;
+        if (tt == null) {
+            // If this is non-null, we know that tail != head
+            return null;
+        }
+        this.tail = tt;
         return tt.item;
     }
 
-    public void push(T t) {
-        Node<T> nh = new Node<T>();
+    /**
+     * Pushes an item to the queue.
+     * @param t The item to push.
+     */
+    public void push(final T t) {
+        final Node<T> nh = new Node<>();
         nh.item = t;
-        head.prev = nh;
-        head = nh;
+        this.head.prev = nh;
+        this.head = nh;
     }
 
     private class Node<NT> {
         private Node<NT> prev;
         private NT item;
     }
+
 }
