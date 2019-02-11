f90ccac97216e13d69c5adece977b12dc1aa4d9e
Avahe Kellenberger <avahe@protonmail.ch>
2019-02-11 14:55:04 -0500

    Added InfoPanes to the InfoLayer.
    InfoPanes are used to render information atop the game and game objects.

--- client/src/ts/icepush/scene/game/InfoLayer.ts
+++ client/src/ts/icepush/scene/game/InfoLayer.ts
@@ -1,8 +1,75 @@
 import {Layer} from "../../../engine/game/Layer";
 import {Vector2D} from "../../../engine/math/Vector2D";
+import {Renderable} from "../../../engine/game/ui/Renderable";
+import {ILocatable, Locatable} from "../../../engine/game/entity/Locatable";
+
+export class InfoPane implements ILocatable, Locatable, Renderable {
+
+    private image: HTMLImageElement;
+    private location: Vector2D;
+
+    /**
+     * @param image The image to display.
+     * @param location The location to render the pane.
+     */
+    constructor(image: HTMLImageElement, location: Vector2D = Vector2D.ZERO) {
+        this.image = image;
+        this.location = location;
+    }
+
+    /**
+     * @override
+     */
+    public getLocation(): Vector2D {
+        return this.location;
+    }
+
+    /**
+     * @override
+     */
+    public setLocation(loc: Vector2D): boolean {
+        if (this.location.equals(loc)) {
+            return false;
+        }
+        this.location = loc;
+        return true;
+    }
+
+    /**
+     * @override
+     */
+    public translate(delta: Vector2D): void {
+        this.setLocation(this.location.addVector(delta));
+    }
+
+    /**
+     * @return The image to render.
+     */
+    public getImage(): HTMLImageElement {
+        return this.image;
+    }
+
+    /**
+     * Sets the image to be rendered.
+     * @param image The image to set.
+     */
+    public setImage(image: HTMLImageElement): void {
+        this.image = image;
+    }
+
+    /**
+     * @override
+     */
+    public render(ctx: CanvasRenderingContext2D): void {
+        ctx.drawImage(this.image, this.location.x - this.image.width * 0.5,
+                                  this.location.y - this.image.height * 0.5);
+    }
+
+}
 
 export class InfoLayer extends Layer {
 
+    private readonly infoPaneMap: Map<number, InfoPane>;
     private readonly timeRenderLocation: Vector2D;
 
     private roundSecondsRemaining: number|undefined;
@@ -14,8 +81,27 @@ export class InfoLayer extends Layer {
      */
     constructor(timeRenderLocation: Vector2D, zOrder: number = 1) {
         super(zOrder);
+        this.infoPaneMap = new Map();
         this.timeRenderLocation = timeRenderLocation;
         this.isRoundCountingDown = false;
+    }
+
+    /**
+     * Sets the pane associated with the object.
+     * @param objectID The ID of the object associated with the info pane.
+     * @param infoPane The InfoPane to associate with the objectID.
+     */
+    public setObjectInfoPane(objectID: number, infoPane: InfoPane): void {
+        this.infoPaneMap.set(objectID, infoPane);
+    }
+
+    /**
+     * Removes the info pane associated with the object ID.
+     * @param objectID The ID of the object.
+     * @return If the pane existed and was removed.
+     */
+    public removeObjectInfoPane(objectID: number): boolean {
+        return this.infoPaneMap.delete(objectID);
     }
 
     /**
@@ -38,6 +124,9 @@ export class InfoLayer extends Layer {
      */
     public render(ctx: CanvasRenderingContext2D): void {
         super.render(ctx);
+
+        // Render the info panes.
+        this.infoPaneMap.forEach(pane => pane.render(ctx));
 
         if (this.roundSecondsRemaining !== undefined) {
             this.renderRoundTimeRemaining(ctx, this.roundSecondsRemaining);
